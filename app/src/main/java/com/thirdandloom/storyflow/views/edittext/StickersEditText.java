6fffc6f297307bb36d866aee34ba6e5eb5ce1e73
Artem Tkachenko <artm.tkachenko@gmail.com>
2016-04-30 22:13:24 +0300

    add delete and copy and paste functionality for Cats Stickers

--- app/src/main/java/com/thirdandloom/storyflow/views/edittext/StickersEditText.java
+++ app/src/main/java/com/thirdandloom/storyflow/views/edittext/StickersEditText.java
@@ -2,6 +2,7 @@ package com.thirdandloom.storyflow.views.edittext;
 
 import com.rockerhieu.emojicon.EmojiconEditText;
 import com.thirdandloom.storyflow.Theme;
+
 import rx.functions.Action0;
 import rx.functions.Action1;
 
@@ -20,7 +21,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StickersEditText extends EmojiconEditText {
-    private static final Pattern FIND_IMAGE_REG_EXP = Pattern.compile("\\[(.*?)\\]");
+    private static final String FIND_REG_EXP = "\\[(\\w*?)\\]";
+    private static final String FIND_DELETED_REG_EXP = "\\[\\s*(\\w+)*$|\\[\\s*(\\w+)*\\s|\\[\\s*(\\w+)*[\\[]";
+    private static final Pattern FIND_IMAGE_PATTERN = Pattern.compile(FIND_REG_EXP);
+    private static final Pattern FIND_DELETE_IMAGE_PATTERN = Pattern.compile(FIND_DELETED_REG_EXP);
+
+    private static List<DisplayedSticker> oldDetectedStickers = new ArrayList<>();
 
     public StickersEditText(Context context) {
         this(context, null);
@@ -40,17 +46,47 @@ public class StickersEditText extends EmojiconEditText {
     }
 
     @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+        boolean selectionWasModified = false;
+
+        if (selStart != selEnd) {
+            String currentText = getText().toString();
+            String selectedText = currentText.substring(selStart, selEnd);
+            boolean lastSymbolIsStartSticker = selectedText.charAt(selectedText.length() - 1) == "[".charAt(0);
+            boolean firstSymbolIsEndSticker = selectedText.charAt(0) == "]".charAt(0);
+
+            if (lastSymbolIsStartSticker) {
+                String detectedSticker = getStickerWithStartStickerPos(selEnd - 1);
+                selectionWasModified = detectedSticker.length() > 1;
+                selEnd = selEnd + detectedSticker.length() - 1;
+            }
+            if (firstSymbolIsEndSticker) {
+                String detectedSticker = getStickerWithEndStickerPos(selStart + 1);
+                if (detectedSticker.length() > 1) {
+                    selectionWasModified = true;
+                    selStart = selStart + 1;
+                }
+            }
+        }
+        if (!selectionWasModified) {
+            super.onSelectionChanged(selStart, selEnd);
+        } else {
+            setSelection(selStart, selEnd);
+        }
+    }
+
+    @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        int oldTextLength = getText().length();
+        int oldSelectionEnd = getSelectionEnd();
         getTextWithImages(getText(), newText -> {
             setText(newText);
-            setSelection(newText.length());
+            setSelection(oldSelectionEnd-(oldTextLength - newText.length()));
         }, () -> super.onTextChanged(text, start, lengthBefore, lengthAfter));
     }
 
-
-    private static List<String> oldDetectedStickers = new ArrayList<>();
     private static void getTextWithImages(Editable text, Action1<Editable> onChanged, Action0 onNotChanged) {
-        Matcher matcher = FIND_IMAGE_REG_EXP.matcher(text.toString());
+        Matcher matcher = FIND_IMAGE_PATTERN.matcher(text.toString());
 
         ImageSpan[] oldSpans = text.getSpans(0, text.length(), ImageSpan.class);
         List<ImageSpan> oldSpansList = new ArrayList<>(new ArrayList<>(Arrays.asList(oldSpans)));
@@ -58,34 +94,100 @@ public class StickersEditText extends EmojiconEditText {
             text.removeSpan(span);
         }
 
-        List<String> detectedStickers = new ArrayList<>();
+        List<DisplayedSticker> detectedStickers = new ArrayList<>();
         while (matcher.find()) {
             String sticker = matcher.group();
             if (Theme.Stickers.catMap.containsKey(sticker)) {
-                detectedStickers.add(sticker);
+                int start = matcher.start();
+                int end = matcher.end();
+                detectedStickers.add(new DisplayedSticker(start, end, sticker));
                 Drawable image = Theme.Stickers.catMap.get(sticker);
                 ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BASELINE);
                 text.setSpan(imageSpan,
-                        matcher.start(),
-                        matcher.end(),
+                        start,
+                        end,
                         Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
                 );
             }
         }
 
-        oldDetectedStickers.removeAll(detectedStickers);
-        boolean removedStickersDetected = oldDetectedStickers.size() != 0;
-        if (removedStickersDetected) {
-            for (String removedSticker : oldDetectedStickers) {
-                String removed = removedSticker.replace("]", "");
-                text = new SpannableStringBuilder(text.toString().replace(removed, ""));
+        boolean removedStickersDetected = false;
+        if (oldDetectedStickers.size() > detectedStickers.size()) {
+            Matcher deletedMatcher = FIND_DELETE_IMAGE_PATTERN.matcher(text.toString());
+            while (deletedMatcher.find()) {
+                String sticker = deletedMatcher.group();
+                boolean isPart = isPart(sticker);
+                if (isPart) {
+                    removedStickersDetected = true;
+                    StringBuilder stringBuilder = new StringBuilder(text);
+                    if (sticker.charAt(sticker.length() - 1) == "[".charAt(0)) {
+                        stringBuilder.replace(deletedMatcher.start() + 1, deletedMatcher.end(), "");
+                    } else {
+                        stringBuilder.replace(deletedMatcher.start(), deletedMatcher.end(), "");
+                    }
+                    text = new SpannableStringBuilder(stringBuilder);
+                }
             }
         }
+
         oldDetectedStickers = detectedStickers;
         if (removedStickersDetected) {
             onChanged.call(text);
         } else {
             onNotChanged.call();
+        }
+    }
+
+
+    /**
+     *
+     * @param endPosition
+     * @return whitespace if not detected
+     */
+    private static String getStickerWithEndStickerPos(int endPosition) {
+        for (DisplayedSticker sticker : oldDetectedStickers) {
+            if (sticker.endPosition == endPosition) {
+                return sticker.text;
+            }
+        }
+        return " ";
+    }
+
+    /**
+     *
+     * @param startPosition
+     * @return whitespace if not detected
+     */
+    private static String getStickerWithStartStickerPos(int startPosition) {
+        for (DisplayedSticker sticker : oldDetectedStickers) {
+            if (sticker.startPosition == startPosition) {
+                return sticker.text;
+            }
+        }
+        return " ";
+    }
+
+    private static boolean isPart(String sticker) {
+        if (sticker.charAt(sticker.length() - 1) == "[".charAt(0)) {
+            sticker = sticker.substring(0, sticker.length() - 2);
+        }
+        for (String currentSticker : Theme.Stickers.catMapKeysList) {
+            if (currentSticker.contains(sticker)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class DisplayedSticker {
+        public int startPosition;
+        public int endPosition;
+        public String text;
+
+        public DisplayedSticker(int startPos, int endPos, String text) {
+            this.startPosition = startPos;
+            this.endPosition = endPos;
+            this.text = text;
         }
     }
 }
